677727f61bc041ba73911d5a4edc12840610d61e
CharaD7 <jijakahn6@gmail.com>
2024-01-15 14:38:40 +0000

    Create sanity client

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -3,3 +3,10 @@ import { createClient } from "@sanity/client";
 const projectId = "z3v20xj4";
 const dataset = "production";
 const apiVersion = "2022-03-07";
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true,
+});
