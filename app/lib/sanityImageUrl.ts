504879fa5db865a8862616379f11af13fc65839b
CharaD7 <jijakahn6@gmail.com>
2024-01-16 13:20:44 +0000

    Created image url builder

---
+++ app/lib/sanityImageUrl.ts
@@ -0,0 +1,10 @@
+import ImageUrlBuilder from "@sanity/image-url";
+
+import { client } from "./sanity";
+
+const builder = ImageUrlBuilder(client);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function urlFor(source: any) {
+  return builder.image(source);
+}
