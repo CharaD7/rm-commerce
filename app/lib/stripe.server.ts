e391c1d158566b88471647e95411fc581fc80564
CharaD7 <jijakahn6@gmail.com>
2024-01-17 18:55:44 +0000

    Implemented stripe server page

--- app/lib/stripe.server.ts
+++ app/lib/stripe.server.ts
@@ -1,3 +1,7 @@
+import Stripe from "stripe";
+
+import { ProductId } from "./interface";
+
 export function getDomainUrl(request: Request) {
   const host =
     request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
@@ -10,3 +14,34 @@ export function getDomainUrl(request: Request) {
 
   return `${protocol}://${host}`;
 }
+
+export const getStripeSession = async (
+  items: string,
+  domainUrl: string,
+): Promise<string> => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+    apiVersion: "2023-10-16",
+    typescript: true,
+  });
+
+  const dataObject = JSON.parse(items);
+
+  const lineItems = dataObject.map((product: ProductId) => {
+    return {
+      price: product.stripeProductId,
+      quantity: product.quantity,
+      adjustable_quantity: { enabled: true, minimum: 1, maximum: 10 },
+    };
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    mode: "payment",
+    payment_method_types: ["card"],
+    line_items: lineItems,
+    success_url: `${domainUrl}/payment/success`,
+    // success_url: `${domainUrl}/payment/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${domainUrl}/payment/canceled`,
+  });
+
+  return session.url as string;
+};
